87c64866f7c27ad14b59be0c9a2a64b5dc7b4946
FacundoLopezz <frlopez007@gmail.com>
2021-08-22 17:14:53 -0300

    1

--- js/init.js
+++ js/init.js
@@ -40,8 +40,13 @@ var getJSONData = function(url){
     });
 }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+function signOut(){
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function(){
+  });
+  localStorage.clear(); ///Borra toooodo el localStorage
+  location.href="index.html"; //De donde esté, no importa dónde, me envía a index.html
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
 });
