d105e3300e6e3988abca324eb59135e81bfde3ac
FacundoLopezz <frlopez007@gmail.com>
2021-11-28 22:03:33 -0300

    1

--- js/init.js
+++ js/init.js
@@ -1,12 +1,17 @@
-const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const CATEGORIES_URL = "http://localhost:3000/lista";
 const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
 const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
-const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
-const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const PRODUCTS_URL = "http://localhost:3000/productos";
+const PRODUCT_INFO_URL = "http://japdevdep.github.io/ecommerce-api/product/5678.json";
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
-const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+const CART_INFO_URL = "http://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_BUY_URL = "http://localhost:3000/cart";
 const CARRO="https://japdevdep.github.io/ecommerce-api/cart/654.json";
+
+
+/*const CART_BUY_URL = "http://localhost:3000/cart";
+const PRODUCTS_URL = "http://localhost:3000/productos";
+const CATEGORIES_URL = "http://localhost:3000/lista"; */
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
